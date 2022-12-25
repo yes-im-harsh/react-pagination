7ca03ffac65d9bef59dccad620ff3be6cc682499
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-12-25 23:46:15 +0530

    Project Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import PaginationLink from "./components/PaginationLink";
+import Post from "./components/Post";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostPerPage] = useState(10);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetchList();
+  }, []);
+
+  const fetchList = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await response.json();
+    setPosts(data);
+  };
+
+  //Get Current Posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  //change page Number
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <h1 className="text-primary ">API Data</h1>
+      <Post posts={currentPosts} />
+      <PaginationLink
+        postsPerPage={postsPerPage}
+        totalPosts={posts.length}
+        paginate={paginate}
+      />
     </div>
   );
 }
---
+++ src/components/Post.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Post = ({ posts }) => {
+  return (
+    <ul className="list-group mb-4">
+      {posts.map((post) => (
+        <li key={post.id} className="list-group-item">
+          {post.title}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Post;
